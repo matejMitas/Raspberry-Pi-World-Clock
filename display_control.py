db1a798086204a079a9b0cbd20104e12cd4f45ef
Matěj Mitaš <matej.mitas@gmail.com>
2015-02-07 12:44:28 +0100

    MUX preparation
    Added MUX pins, Digit as string

--- display_control.py
+++ display_control.py
@@ -7,12 +7,18 @@ from time import sleep
 #BCM - numbers of GPIOs
 G.setmode(G.BCM)
 
-#Actual numbers of pins, alphabetical order
-pins = [7,8,11,10,22,25,9]
+#Actual numbers of diplay pins, [a,b,c,d,e,f,g]
+display_pins = [2,3,17,27,22,10,4]
 
-#Set pins to output
-for pin in pins:
-	G.setup(pin, G.OUT)	
+#MUX pins, 11 - first digit, 9 - second digit
+mux_pins = [11,9]
+
+#Set dislay pins to output
+for display_pin in display_pins:
+	G.setup(display_pin, G.OUT)	
+
+G.setup(11, G.OUT)
+G.setup(9, G.OUT)		
 
 #Define states of pins
 zero = 		[1,1,1,1,1,1,0]
@@ -23,33 +29,36 @@ four = 		[0,1,1,0,0,1,1]
 five = 		[1,0,1,1,0,1,1]
 six = 		[1,0,1,1,1,1,1]
 seven = 	[1,1,1,0,0,0,0]
-eight = 		[1,1,1,1,1,1,1]
+eight = 	[1,1,1,1,1,1,1]
 nine = 		[1,1,1,1,0,1,1]
+
+empty_char =[0,0,0,0,0,0,0]
+minus =		[0,0,0,0,0,0,1]
 
 #Match values with numbers
 values = {
-		0 : zero,
-		1 : one,
-		2 : two,
-		3 : three,
-		4 : four,
-		5 : five,
-		6 : six,
-		7 : seven,
-		8 : eight,
-		9 : nine
+		"0" : zero,
+		"1" : one,
+		"2" : two,
+		"3" : three,
+		"4" : four,
+		"5" : five,
+		"6" : six,
+		"7" : seven,
+		"8" : eight,
+		"9" : nine,
+		"-" : minus,
+		" " : empty_char
 	}
 
 #main function
-def seven_segment(number, polarity = "anode"):
+def seven_segment(number, empty_char = False, polarity = "anode"):
 	
-	for pin, level in zip(pins, values[number]):
+	for pin, level in zip(display_pins, values[number]):
 		G.output(pin, not level)
 		
 	if polarity == "catode":
-		for pin, level in zip(pins, values[number]):
+		for pin, level in zip(display_pins, values[number]):
 				G.output(pin, level)
 
-#usage - seven_segment(your_number)
-	
-G.cleanup()
+seven_segment("8")
